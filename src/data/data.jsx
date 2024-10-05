f19a02c08d8a17eff103b233b9990ae515fb914d
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-10-05 08:22:04 +0530

    Create data.jsx

---
+++ src/data/data.jsx
@@ -0,0 +1,129 @@
+export const menData = [
+    {
+      id: 1,
+      name: "Casual Shirt",
+      price: "$2500",
+      image: "https://5.imimg.com/data5/SELLER/Default/2023/12/370579566/DO/DE/GU/161283728/men-plain-shirts-men-shirt.jpg"
+    },
+    {
+      id: 2,
+      name: "Formal Shirt",
+      price: "$3000",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuStK0SUD0J8x844g7pxyHMSR9ZnacI2m8lQ&s"
+    },
+    {
+      id: 3,
+      name: "Denim Jacket",
+      price: "$3550",
+      image: "https://img.lazcdn.com/g/p/9408d970f863eda0cb6e87d5e69d5692.jpg_720x720q80.jpg"
+    },
+    // Add more items as needed
+    {
+        image_url:
+          "https://images.jdmagicbox.com/quickquotes/images_main/reebok-running-shoes-04-03-2022-3944-270837835-f7ojfko5.jpg",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        price: "1170",
+        strikedoffprice: "$2599",
+        
+      },
+      {
+        image_url:
+          "https://images-static.nykaa.com/media/catalog/product/6/8/68df6f1GA2088_1.jpg",
+        name: "MEN'S REEBOK SWIM ARUBA FLIP SLIPPERS ",
+        price: 699,
+        strikedoffprice: 999,
+      },
+      {
+        image_url:
+          "https://sbuyimages.dzvdesk.com/Uploads/Reebok%20Mens%20Austin%20Running%20Shoe%204_2023_04_24_15_59_57.jpg",
+        name: " MEN'S REEBOK RUNNING AUSTIN SHOES",
+        price: 945,
+        strikedoffprice: 2099,
+      },
+      {
+        image_url:
+          "https://rukminim2.flixcart.com/image/850/1000/xif0q/slipper-flip-flop/j/d/h/7-gb2164-reebok-grey-original-imagh2qhshfwbmgr.jpeg?q=90&crop=false",
+        name: "MEN'S REEBOK SWIM AVENGER FLIP LP SLIPPERS",
+        price: 399,
+        strikedoffprice: 799,
+      },
+      {
+        image_url:
+          "https://images.jdmagicbox.com/quickquotes/images_main/reebok-men-s-fast-tempo-flexweave-running-shoes-multicolor-164849899-h2sx6.jpg",
+        name: "MEN'S REEBOK RUNNING TEMPO SHOES ",
+        price: 1620,
+        strikedoffprice: 3599,
+      },
+      {
+        image_url:
+          "https://images.jdmagicbox.com/quickquotes/images_main/reebok-mens-footwear-01-07-2021-405-237552126-7px5r.jpg",
+        name: " MEN'S REEBOK RUNNING ASTRO BOOSTER SHOES ",
+        price: 1440,
+        strikedoffprice: 3199,
+      },
+      {
+        image_url: 
+        "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/76c46f536185476984a47d3525ccac7e_9366/Designed_for_Training_Workout_Pants_Black_IK9686_21_model.jpg",
+        name: " MEN'S TRAINING WORKOUT READY ELITAGE PANTS ",
+        price: 1999,
+        strikedoffprice: 3199,
+      },
+      {
+        image_url:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMQXaegs2t_5i81wxIwfClE6YVzD2QDF_hDg&s",
+        name: "MEN'S REEBOK TRAINING  POLO T-SHIRTS TEE  ",
+        price: 769,
+        strikedoffprice: 1099,
+      },
+      {
+        image_url:
+          "https://prod.aaw.com/media/catalog/product/6/2/62972420d2b44ed6ebdccb6581de29a6f406b4a3bacc9c424bf6b2a0161188ec.jpeg",
+        name: "MEN'S REEBOK TRAINING WORKOUT  TEE  ",
+        price: 1299,
+        strikedoffprice: 1440,
+      },
+      {
+        image_url:
+          "https://images-static.nykaa.com/media/catalog/product/6/8/68df6f1EY2843_1.jpg?tr=w-500 ",
+        name: "MEN'S REEBOK SUPER SOFT FLIP SLIPPERS",
+        price: 699,
+        strikedoffprice: 999,
+      },
+      {
+        image_url:
+          "https://images.jdmagicbox.com/quickquotes/images_main/reebok-running-shoes-04-03-2022-3944-270837835-f7ojfko5.jpg ",
+        name: "MEN'S REEBOK RUNNING RUN SHOES  ",
+        price: 1620,
+        strikedoffprice: 3599,
+      },
+      {
+        image_url:
+          "https://images.jdmagicbox.com/quickquotes/images_main/reebok-women-s-genesis-runner-running-shoes-white-gold-metallic-164859620-fui4k.jpg ",
+        name: "MEN'S REEBOK RUNNING GENESIS RUNNER SHOES  ",
+        price: 1620,
+        strikedoffprice: 3599,
+      }
+  ];
+  
+  export const womenData = [
+    {
+      id: 1,
+      name: "Summer Dress",
+      price: "$1500",
+      image: "https://images.meesho.com/images/products/379543377/81vcz_512.webp"
+    },
+    {
+      id: 2,
+      name: "Blazer",
+      price: "$4500",
+      image: "https://assets.ajio.com/medias/sys_master/root/20240725/VvVa/66a1a8641d763220fa235945/-473Wx593H-700227039-burgundy-MODEL4.jpg"
+    },
+    {
+      id: 3,
+      name: "Casual Top",
+      price: "$2000",
+      image: "https://assets.ajio.com/medias/sys_master/root/20240611/MYEC/6668335816fd2c6e6a8d889d/-473Wx593H-469451443-black-MODEL.jpg"
+    },
+    // Add more items as needed
+  ];
+  
